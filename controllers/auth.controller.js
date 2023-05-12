a0690132a91947d2d7e37a567335ce2541e1977d
Beatriz <beatriz.sfrodrigues@gmail.com>
2023-05-12 16:59:17 +0100

    ok

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,7 @@
+const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcryptjs");
+const config = require("../config/db.config.js");
+// const db = require("../models");
 // const encodedHeader = base64urlEncode(header);
 
 // const encodedPayload = base64urlEncode(payload);
@@ -10,21 +14,21 @@
 // const JWT = encodedHeader + "." + encodedPayload + "." + signature;
 
 exports.verifyToken = (req, res, next) => {
-    // search token in headers most commonly used for authorization
-    const header = req.headers['x-access-token'] || req.headers.authorization;
-    
-    if (typeof header == 'undefined')
-        return res.status(401).json({ success: false, msg: "No token provided!" });
-    
-    const bearer = header.split(' '); // Authorization: Bearer <token>
-    const token = bearer[1];
-    
-    try {
-        let decoded = jwt.verify(token, config.SECRET);
-        req.loggedUserId = decoded.id; // save user ID and role into request object
-        req.loggedUserRole = decoded.role;
-        next();
-    } catch (err) {
-        return res.status(401).json({ success: false, msg: "Unauthorized!" });
-    }
+  // search token in headers most commonly used for authorization
+  const header = req.headers["x-access-token"] || req.headers.authorization;
+
+  if (typeof header == "undefined")
+    return res.status(401).json({ success: false, msg: "No token provided!" });
+
+  const bearer = header.split(" "); // Authorization: Bearer <token>
+  const token = bearer[1];
+
+  try {
+    let decoded = jwt.verify(token, config.SECRET);
+    req.loggedUserId = decoded.id; // save user ID and role into request object
+    req.loggedUserType = decoded.type;
+    next();
+  } catch (err) {
+    return res.status(401).json({ success: false, msg: "Unauthorized!" });
+  }
 };
