ba80d643f541ab7aaa5270eb9e0b0ac8e608274b
Daniela Moreira <40210349@esmad.ipp.pt>
2023-05-26 14:28:14 +0100

    ..

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -259,7 +259,7 @@ exports.deleteUser = async (req, res) => {
           message: "User does not exist",
         });
       } else {
-        return res.status(200).json({
+        return res.status(204).json({
           success: true,
           message: `User with id ${req.params.userID} was deleted successfully`,
         });
@@ -293,7 +293,7 @@ exports.blockUser = async (req, res) => {
         await user.save();
         return res.status(200).json({
           success: true,
-          message: "User was blocked successfully",
+          message: "User state was changed successfully",
           user: user,
         });
       }
