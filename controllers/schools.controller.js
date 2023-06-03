b126524f55f9da6d126e67502005764019de57f8
Beatriz <beatriz.sfrodrigues@gmail.com>
2023-06-03 14:26:51 +0100

    ai

---
+++ controllers/schools.controller.js
@@ -0,0 +1,50 @@
+const db = require("../models");
+const School = db.schools;
+
+exports.findAll = async (req, res) => {
+  try {
+    console.log("aii");
+    if (req.loggedUser) {
+      let schools = await School.find({}).exec();
+      res.status(200).json({ success: true, schools: schools });
+    } else {
+      return res.status(401).json({
+        success: false,
+        msg: "You have to be logged in",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      msg:
+        err.message || "Some error occurred while retrieving all occurrences.",
+    });
+  }
+};
+
+// exports.findOne = async (req, res) => {
+//   try {
+//     if (req.loggedUser.type == "user") {
+//       return res.status(403).json({
+//         success: false,
+//         msg: "This request requires ADMIN/SECURITY role!",
+//       });
+//     } else {
+//       let occurrence = await Occurrence.findById(req.params.occID);
+//       if (occurrence === null) {
+//         return res.status(404).json({
+//           success: false,
+//           msg: `Cannot find any occurrence with ID ${req.params.occID}`,
+//         });
+//       } else {
+//         res.status(200).json({ success: true, occurrence: occurrence });
+//       }
+//     }
+//   } catch (err) {
+//     res.status(500).json({
+//       success: false,
+//       msg:
+//         err.message || "Some error occurred while retrieving this occurrence.",
+//     });
+//   }
+// };
